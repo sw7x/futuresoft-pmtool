cc129e7893fca689558ff2dc9b6dccf211618705
sw7x <susanthawarnapura@gmail.com>
2025-05-24 21:18:57 +0530

    add artisan command migrate:in-order

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * The Artisan commands provided by application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        Commands\MigrateInOrder::class,
+    ];
+
+    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
