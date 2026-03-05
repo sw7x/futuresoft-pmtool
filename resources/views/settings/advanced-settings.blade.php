b9cbf7f62a35454e1a96772e7387f685ff5252d2
sw7x <susanthawarnapura@gmail.com>
2026-03-05 15:20:29 +0530

    create settings pages

--- resources/views/settings/advanced-settings.blade.php
+++ resources/views/settings/advanced-settings.blade.php
@@ -36,8 +36,91 @@
 
             <div class="ibox">
                 <div class="ibox-content">                        
-                
+                    <form method="POST" action="#" class="form-horizontal">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Mail Host</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="mail_host" value="smtp.mailtrap.io">
+                                <span class="form-text m-b-none text-muted">Hostname of the SMTP server.</span>
+                            </div>
+                        </div>
 
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Mail Port</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" name="mail_port" value="2525">
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Mail Encryption</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="mail_encryption">
+                                    <option value="tls" selected>TLS</option>
+                                    <option value="ssl">SSL</option>
+                                    <option value="none">None</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Maintenance Mode</label>
+                            <div class="col-sm-8">
+                                <div class="i-checks">
+                                    <label>
+                                        <input type="checkbox" value="1" name="maintenance_mode"> 
+                                        <i></i> Enable Maintenance Mode
+                                    </label>
+                                </div>
+                                <span class="form-text m-b-none text-danger small">Turning this on will prevent all non-admin users from accessing the site.</span>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Session Lifetime (Min)</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" name="session_lifetime" value="120">
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Max File Upload Size (MB)</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" name="max_file_size" value="10">
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Auto-Archive Tasks (Days)</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" name="archive_days" value="90">
+                                <span class="form-text m-b-none text-muted">Number of days after which completed tasks are archived.</span>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <div class="col-sm-4 offset-sm-4">
+                                <button class="btn btn-primary btn-sm" type="submit">Save Advanced Settings</button>
+                                <button class="btn btn-danger btn-sm" type="reset">Cancel</button>
+                            </div>
+                        </div>
+                        
+                    </form>
                 </div>
             </div>
         
--- resources/views/settings/general-settings.blade.php
+++ resources/views/settings/general-settings.blade.php
@@ -36,8 +36,141 @@
 
             <div class="ibox">
                 <div class="ibox-content">                        
-                
+                    <form method="POST" action="#" class="form-horizontal">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">App Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="app_name" value="FutureSoft PM Tool">
+                                <span class="form-text m-b-none text-muted">The name of your application as it appears to users.</span>
+                            </div>
+                        </div>
 
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Default Language</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="default_language">
+                                    <option value="en" selected>English (UK)</option>
+                                    <option value="us">English (US)</option>
+                                    <option value="es">Spanish</option>
+                                    <option value="fr">French</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Default Currency</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="default_currency">
+                                    <option value="INR" selected>INR (₹) - Indian Rupee</option>
+                                    <option value="USD">USD ($) - US Dollar</option>
+                                    <option value="EUR">EUR (€) - Euro</option>
+                                    <option value="GBP">GBP (£) - British Pound</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Week Starts On</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="week_start">
+                                    <option value="Monday" selected>Monday</option>
+                                    <option value="Sunday">Sunday</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Timezone</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="timezone">
+                                    <option value="Asia/Kolkata" selected>Asia/Kolkata (GMT+05:30)</option>
+                                    <option value="UTC">UTC (GMT+00:00)</option>
+                                    <option value="America/New_York">America/New_York (GMT-05:00)</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Date Format</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="date_format">
+                                    <option value="d/m/Y" selected>DD/MM/YYYY (31/12/2026)</option>
+                                    <option value="m/d/Y">MM/DD/YYYY (12/31/2026)</option>
+                                    <option value="Y-m-d">YYYY-MM-DD (2026-12-31)</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Company Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="company_name" value="FutureSoft Solutions">
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Company Address</label>
+                            <div class="col-sm-8">
+                                <textarea class="form-control" name="company_address" rows="3">Sector 18, Gurgaon, Haryana, India</textarea>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Admin Email</label>
+                            <div class="col-sm-8">
+                                <input type="email" class="form-control" name="admin_email" value="admin@futuresoft.com">
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Registration</label>
+                            <div class="col-sm-8">
+                                <div class="i-checks">
+                                    <label>
+                                        <input type="checkbox" value="1" name="allow_registration" checked> 
+                                        <i></i> Allow new users to register
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label font-bold">Footer Copyright Text</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="footer_text" value="&copy; 2026 FutureSoft Solutions Pvt. Ltd.">
+                            </div>
+                        </div>
+
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group row">
+                            <div class="col-sm-4 offset-sm-4">
+                                <button class="btn btn-primary btn-sm" type="submit">Save settings</button>
+                                <button class="btn btn-danger btn-sm" type="reset">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         
